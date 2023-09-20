963727bd75f09ee250cc700fa4f224a20ffb67a6
toleman84 <gdponcemarsiglia@gmail.com>
2023-09-20 13:54:22 -0300

    add ex.5

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+
+function storeResponse(url, filename) {
+  request(url, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      fs.writeFile(filename, body, 'utf-8', function (err) {
+        if (err) {
+          console.log('Error writing file:', err);
+        } else {
+          console.log('File written successfully.');
+        }
+      });
+    }
+  });
+}
+storeResponse(process.argv[2], process.argv[3]);
