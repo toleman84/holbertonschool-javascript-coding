1ef0272e6981f8623148e86c717918e1a9c75ed0
toleman84 <gdponcemarsiglia@gmail.com>
2023-09-20 14:03:03 -0300

    add exercise 6

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const request = require('request');
+function countCompletedTasks(url) {
+  request(url, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      const todos = JSON.parse(body);
+      const completedTasks = todos.filter(todo => todo.completed);
+      const userIds = completedTasks.map(todo => todo.userId);
+      const taskCounts = userIds.reduce((counts, userId) => {
+        counts[userId] = (counts[userId] || 0) + 1;
+        return counts;
+      }, {});
+      console.log(taskCounts);
+    }
+  });
+}
+countCompletedTasks(process.argv[2]);
