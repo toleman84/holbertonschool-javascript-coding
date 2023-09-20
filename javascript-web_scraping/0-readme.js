abbcc04ddcd9ce0daee7bb3abadd45c4f9153bb1
toleman84 <gdponcemarsiglia@gmail.com>
2023-09-20 14:06:11 -0300

    fix ex.0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const fs = require("fs");
+const fs = require('fs');
 const filePath = process.argv[2];
 try {
-  const data = fs.readFileSync(filePath, "utf-8");
+  const data = fs.readFileSync(filePath, 'utf-8');
   console.log(data);
 } catch (err) {
   console.error(err);
