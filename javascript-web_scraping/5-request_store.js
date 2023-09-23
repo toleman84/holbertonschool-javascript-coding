d8a7b798b367a8c5dfc22d9fbcbe352458101076
toleman84 <gdponcemarsiglia@gmail.com>
2023-09-23 01:18:59 -0300

    fix ex.5

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -2,17 +2,16 @@
 const request = require('request');
 const fs = require('fs');
 
-function storeResponse(url, filename) {
-  request(url, function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-      fs.writeFile(filename, body, 'utf-8', function (err) {
-        if (err) {
-          console.log('Error writing file:', err);
-        } else {
-          console.log('File written successfully.');
-        }
-      });
-    }
-  });
-}
-storeResponse(process.argv[2], process.argv[3]);
+const url = process.argv[2];
+const path = process.argv[3];
+
+request(url, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    fs.writeFile(path, body, 'utf-8', function (err) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+    });
+  }
+});
