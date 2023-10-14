ae11d1870b83f301ffe68477876f56bc46e289d7
toleman84 <gdponcemarsiglia@gmail.com>
2023-10-13 22:58:12 -0300

    add exercise 4

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write('Hello Holberton School!');
+  res.end();
+});
+
+app.listen(1245, () => {
+  console.log('Server is running on port 1245');
+});
+
+module.exports = app;
