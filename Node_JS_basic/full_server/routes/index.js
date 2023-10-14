eb0c27b0a55083ae86afdbf53bdd3f90067b9e1e
toleman84 <gdponcemarsiglia@gmail.com>
2023-10-14 00:27:46 -0300

    fix eslint

--- Node_JS_basic/full_server/routes/index.js
+++ Node_JS_basic/full_server/routes/index.js
@@ -8,4 +8,4 @@ router.get('/', AppController.getHomepage);
 router.get('/students', StudentsController.getAllStudents);
 router.get('/students/:major', StudentsController.getAllStudentsByMajor);
 
-module.exports = router
+module.exports = router;
