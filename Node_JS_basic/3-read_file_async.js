d413e6d2f4945e9c0f0f3df5b3f8027db4e08f33
toleman84 <gdponcemarsiglia@gmail.com>
2023-10-14 00:38:39 -0300

    fix ex.3

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,28 +1,47 @@
 const fs = require('fs');
+const { promisify } = require('util');
 
-async function countStudents(path) {
+const readFileAsync = promisify(fs.readFile);
+
+async function countStudents(filePath) {
   try {
-    const data = await fs.promises.readFile(path, 'utf8');
-    const students = data.split('\n').filter((line) => line.trim() !== '');
+    const data = await readFileAsync(filePath, 'utf8');
 
-    const fields = new Set();
-    const studentsByField = new Map();
+    // Split the data into lines.
+    const lines = data.split('\n');
 
-    for (const student of students) {
-      const [field, firstName] = student.split(',');
+    // Create a list of students.
+    const students = [];
+    for (const line of lines) {
+      // Split the line into comma-separated values.
+      const values = line.split(',');
 
-      fields.add(field);
-      studentsByField.set(field, studentsByField.get(field) || []);
-      studentsByField.get(field).push(firstName);
+      // Add the student to the list.
+      students.push({
+        firstName: values[0],
+        major: values[1],
+      });
     }
 
+    // Count the number of students in each major.
+    const studentCountsByMajor = students.reduce((acc, student) => {
+      acc[student.major] = (acc[student.major] || 0) + 1;
+      return acc;
+    }, {});
+
+    // Log the number of students to the console.
     console.log('Number of students:', students.length);
 
-    for (const field of fields) {
-      const studentsInField = studentsByField.get(field);
-      console.log(`Number of students in ${field}: ${studentsInField.length}. List: ${studentsInField.join(', ')}`);
+    // Log the number of students in each field, and the list.
+    for (const [major, count] of Object.entries(studentCountsByMajor)) {
+      const studentList = students.filter((student) => student.major === major).map((student) => student.firstName).join(', ');
+      console.log(`Number of students in ${major}: ${count}. List: ${studentList}`);
     }
+
+    return students.length;
   } catch (error) {
-    throw new Error('Cannot load the database');
+    throw new Error(`Cannot load the database: ${error.message}`);
   }
 }
+
+module.exports = countStudents;
